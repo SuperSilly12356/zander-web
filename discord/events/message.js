6076e3eef4d6448ccde2cc3b42d27a5a4cc05be2
Aidan <69904509+SuperSilly12356@users.noreply.github.com>
2020-09-04 10:23:25 +1000

    make message lower case then check for kenobi

--- discord/events/message.js
+++ discord/events/message.js
@@ -2,9 +2,12 @@ const Discord = require('discord.js');
 const config = require('../../config.json')
 
 module.exports = async message => {
-  const msgcontent = message.content;
-  if (msgcontent.toLowerCase().includes("hello there")) {
-    message.channel.send('General Kenobi');
+  var notLowerCaseKenobi = message.content
+  var lowerCaseKenobi = notLowerCaseKenobi.toLowerCase
+  if (lowerCaseKenobi.includes("hello there")) {
+    let embed = new Discord.MessageEmbed()
+    .attachFiles("https://media.giphy.com/media/8JTFsZmnTR1Rs1JFVP/giphy.gif")
+    message.channel.send(embed);
     return;
   }
 }
